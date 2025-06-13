ed4e900591f5d326e00050b92fc7044ea88803af
DES2048 <electrochange@mail.ru>
2025-06-13 16:48:44 +0300

    refactor upload_photo.go

--- cmd/upload_photo.go
+++ cmd/upload_photo.go
@@ -1,15 +1,14 @@
 package cmd
 
 import (
-	"log"
+	"fmt"
 	"log/slog"
-	"strconv"
-	"vk-cli/auth"
+	"vk-cli/client"
 	"vk-cli/config"
 	"vk-cli/group"
 	"vk-cli/photos"
+	"vk-cli/util"
 
-	"github.com/SevereCloud/vksdk/v2/api"
 	"github.com/spf13/cobra"
 )
 
@@ -22,38 +21,23 @@ var (
 		Use:   "upload-photo",
 		Short: " upload photos(s) to group",
 		Args:  cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			groupId := 0
 
 			// get config
-			config, err := config.ReadConfig(ConfigFile)
+			config := config.GetConfig()
+			// build client
+			vk, err := client.BuildVkClient()
 			if err != nil {
-				log.Fatalf("Failed to load config: %s\n", err)
+				return err
 			}
-
-			// get token
-			token, err := auth.CheckAuth(config.TokenFile)
-			if err != nil {
-				log.Fatalf("Failed to get token:%s\n", err)
-			}
-
-			vk := api.NewVK(token)
-
 			// if group is a group title get group id
 			if len(groupPhotoVar) > 0 {
-				groupId, err = strconv.Atoi(groupPhotoVar)
+				groupId, err = group.GetGroupId(vk, groupPhotoVar)
 				if err != nil {
-					// try to get group id by name
-					group, err := group.GetGroupByName(vk, groupPhotoVar)
-					if err != nil {
-						log.Fatalf("failed to get group by name: %s", err)
-					}
-
-					if group == nil {
-						log.Fatal("group with given name not found")
-					}
-					groupId = group.ID
+					return fmt.Errorf("failed to get group id: %w", err)
 				}
+				// try to get group id by name
 			} else {
 				groupId = config.DefaultGroupID
 			}
@@ -63,13 +47,13 @@ var (
 
 				album, err := photos.GetPhotoAlbumByTitle(vk, -groupId, addPhotoAlbumTitle)
 				if err != nil {
-					log.Fatalf("failed to get albums: %s\n", err)
+					return fmt.Errorf("failed to get albums: %w", err)
 				}
 
 				if album == nil {
 					photoAlbumID, err = photos.AddPhotoAlbum(vk, groupId, addPhotoAlbumTitle)
 					if err != nil {
-						log.Fatalf("Failed to create album: %s\n", err)
+						return fmt.Errorf("failed to create album: %w", err)
 					}
 
 					slog.Info("Added new album", "id", photoAlbumID, "title", addPhotoAlbumTitle)
@@ -79,13 +63,17 @@ var (
 			}
 
 			// upload photos
-			filenames := args
+			filenames, err := util.GetFilenamesFromArgs(args, util.ImageFileExtSet)
+			if err != nil {
+				return fmt.Errorf("failed to get photos from args: %w", err)
+			}
 
 			for idx, filename := range filenames {
 				slog.Info("upload photo", "index", idx+1, "of", len(filenames), "file", filename)
 
 				photos.UploadPhoto(vk, filename, groupId, photoAlbumID)
 			}
+			return nil
 		},
 	}
 )
